d5367d8f552f039e0272ec9e9e277e41f9efff10
Vlad Chernoborodiuk <45739207+Marsianin007@users.noreply.github.com>
2021-11-24 13:54:51 +0100

    Except 0

--- HT_3.7.py
+++ HT_3.7.py
@@ -1,8 +1,8 @@
 # 7. Ну і традиційно -> калькулятор :) повинна бути 1 ф-цiя яка б приймала 3 аргументи - один з яких операцiя, яку зробити!
 
-def calculate(first_num, second_num, operation):
+def calculations(first_num, operation, second_num):
     operation_list = ["+", "-", "/", "*"]
-    if str(first_num).isdigit() and str(second_num).isdigit() and operation in operation_list:
+    if first_num.isdigit() and second_num.isdigit() and operation in operation_list and int(second_num) != 0:
         first_num = int(first_num)
         second_num = int(second_num)
 
@@ -15,11 +15,12 @@ def calculate(first_num, second_num, operation):
         if operation == "/":
             print(str(first_num) + " / " + str(second_num) + " = " + str(first_num / second_num))
     else:
-        print("You can do only this operation(+, -, /, *) with numbers, please, change your input")
+        print("You can do only this operation(+, -, /, *) with numbers except '0', please, change your input")
 
-#operation_list = ["+", "-", "/", "*"]
-#first_num = input("Write first number: ")
-#second_num = input("Write second number: ")
-#operation = input("Write operation to do: ")
+def calculate():
+    first_num = input("Write first number: ")
+    operation = input("Write operation to do: ")
+    second_num = input("Write second number: ")
+    calculations(first_num, operation, second_num)
 
-calculate(5,8,+)
+calculate()
